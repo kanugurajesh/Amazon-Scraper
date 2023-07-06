b71e84d4065c3017f3cf1f6cf910d66218237da3
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-07 03:01:12 +0530

    adder

--- automate.bat
+++ automate.bat
@@ -1,4 +1,7 @@
 @echo off
+
+REM This is a comment use this file in only windows to push code to github
+
 git add .
 git commit -m "adder"
 git push -u origin main
--- automate.sh
+++ automate.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# use this file only in linux to push code to github
 git add .
 git commit -m "adder"
 git push -u origin main
